5adfc3d39a8337197f49038ccfeb1cfc49ca12b0
Markobanduka <137757738+Markobanduka@users.noreply.github.com>
2024-01-04 19:39:35 +0400

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -18,10 +18,10 @@ const Navbar = ()=> {
           {/* Marko Banduka */}
           </motion.span>
         <div className="social">
-          <a href="https://www.facebook.com/marko.banduka.1" target="_blank"><img src="/facebook.png"/></a>
-          <a href="https://www.instagram.com/marko_banduka/?igsh=MXZyM2NoZDFpdmx0MA%3D%3D" target="_blank"><img src="/instagram.png"/></a>
-          <a href="https://wa.link/zjcl4a" target="_blank"><img src="/whatsapp.png"/></a>
-          <a href="https://msng.link/o?381656817481=vi" target="_blank"><img src="/viber.png"/></a>
+          <a href="https://www.facebook.com/marko.banduka.1" target="_blank"><img src="./facebook.png"/></a>
+          <a href="https://www.instagram.com/marko_banduka/?igsh=MXZyM2NoZDFpdmx0MA%3D%3D" target="_blank"><img src="./instagram.png"/></a>
+          <a href="https://wa.link/zjcl4a" target="_blank"><img src="./whatsapp.png"/></a>
+          <a href="https://msng.link/o?381656817481=vi" target="_blank"><img src="./viber.png"/></a>
         </div>
       </div>
     </div>
